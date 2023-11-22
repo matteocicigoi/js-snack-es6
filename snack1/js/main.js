2411b830dbd09f649970510aa043429db47099d8
matteocicigoi <matteo.cicigoi1@hotmail.it>
2023-11-22 14:53:43 +0100

    feat: snack 1

--- snack1/js/main.js
+++ snack1/js/main.js
@@ -1,1 +1,46 @@
 'use strict';
+
+// array 
+const bici = [
+    // oggetto 
+    {
+        nome : 'bici 1',
+        peso : 15
+    },
+    // oggetto 
+    {
+        nome : 'bici 2',
+        peso : 17
+    },
+    // oggetto 
+    {
+        nome : 'bici 3',
+        peso : 14
+    },
+    // oggetto 
+    {
+        nome : 'bici 4',
+        peso : 13
+    },
+    // oggetto 
+    {
+        nome : 'bici 5',
+        peso : 20
+    }
+]
+
+// variabili che salvano la bici che pesa di meno
+let nomeI = null;
+let pesoI = null;
+
+// bici che pesa di meno
+bici.forEach((elemento) => {
+    const {nome , peso} = elemento;
+    if(pesoI === null || peso < pesoI){
+        nomeI = nome;
+        pesoI = peso;
+    }
+});
+
+//stampa il risultato
+document.querySelector('h2').innerText = `La bici che pesa di meno è: ${nomeI} ${pesoI}kg`;
